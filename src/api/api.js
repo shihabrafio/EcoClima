b818e1b1232d6bd0bb5efa82954c3c007e43755b
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-06-23 21:07:25 +0600

    Update api key

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,8 @@
+const api = '4cd28e645dec12995d6b9e36bb63708d';
+
 const fetchLocation = async (ctry) => {
   try {
-    const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${ctry}&limit=1&appid=4cd28e645dec12995d6b9e36bb63708d`);
+    const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${ctry}&limit=1&appid=${api}`);
     const data = await response.json();
     const country = { country: data[0].name, lat: data[0].lat, lon: data[0].lon };
     return (country);
@@ -11,7 +13,7 @@ const fetchLocation = async (ctry) => {
 
 const fetchWeather = async (lat, lon) => {
   try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=4cd28e645dec12995d6b9e36bb63708d`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${api}`);
     const data = await response.json();
     return data;
   } catch (error) {
