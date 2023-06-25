f95b7aabf67d0e1c2af4a7683c0de39124e610be
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-06-26 01:07:46 +0600

    Add Country

---
+++ src/components/Country.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Country = (props) => {
+  const { name, value } = props;
+  const parsedName = React.createElement('div', { dangerouslySetInnerHTML: { __html: name } });
+  const style = { fontWeight: '900', color: '#efefef' };
+  return (
+    <div className="dts-single">
+      {parsedName}
+      <div style={style}>
+        {value}
+      </div>
+    </div>
+  );
+};
+
+export default Country;
