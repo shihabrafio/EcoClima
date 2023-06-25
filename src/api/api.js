80c824026a74e6967ec3c30341874f5751ec9ecb
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-06-26 00:54:06 +0600

    Fix api

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 const apiKey = '4cd28e645dec12995d6b9e36bb63708d';
 
-const getCityIndex = async (lat, lon) => {
+const fetchweather = async (lat, lon) => {
   try {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`);
     const data = await response.json();
@@ -10,7 +10,7 @@ const getCityIndex = async (lat, lon) => {
   }
 };
 
-const getCityCoord = async (ctry) => {
+const fetchcountry = async (ctry) => {
   try {
     const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${ctry}&limit=1&appid=${apiKey}`);
     const data = await response.json();
@@ -20,4 +20,4 @@ const getCityCoord = async (ctry) => {
   }
 };
 
-export { getCityIndex, getCityCoord };
+export { fetchweather, fetchcountry };
