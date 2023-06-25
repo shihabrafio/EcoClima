d15feb83cd545575d3baa2f137fa9022ceb68d63
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-06-26 01:39:26 +0600

    Add countriesdata

--- src/components/main.js
+++ src/components/main.js
@@ -1,26 +1,26 @@
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { getCitiesData } from '../redux/countrySlice';
+import { getCountries } from '../redux/countrySlice';
 import Header from './Header';
 import Countries from './Countries';
 
 const MainPage = () => {
-  const citiesData = useSelector((state) => state.citiesData);
+  const countriesData = useSelector((state) => state.countriesData);
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getCitiesData());
+    dispatch(getCountries());
   }, [dispatch]);
 
   return (
     <div id="main">
       <Header />
       <div className="ctd-container">
-        {citiesData.map((cityData) => (
-          <Link to={`world/${cityData.id}`} key={cityData.id}>
+        {countriesData.map((data) => (
+          <Link to={`world/${data.id}`} key={data.id}>
             <Countries
-              cityName={cityData.city}
-              cityAqi={cityData.data.main.aqi}
+              cityName={data.city}
+              cityAqi={data.data.main.aqi}
             />
           </Link>
         ))}
