72397cb1fb4f2912be087d766b6ac10c808cea42
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-06-26 11:29:08 +0600

    Remove fontWeight

--- src/components/Country.js
+++ src/components/Country.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Country = (props) => {
   const { name, value } = props;
   const parsedName = React.createElement('div', { dangerouslySetInnerHTML: { __html: name } });
-  const style = { fontWeight: '700', color: '#efefef' };
+  const style = { color: '#efefef' };
   return (
     <div className="modal">
       {parsedName}
